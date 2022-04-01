566e7e65166a73c2d885841e8a36da81c7bff1bb
Matt Daniele <2266075+spghtti@users.noreply.github.com>
2022-04-01 13:23:37 -0400

    Improved key generation and added winstate

--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -6,10 +6,11 @@ import fruits from '../fruitList';
 const Gameboard = (props) => {
   const [selectedCards, setSelectedCards] = useState([]);
   const [randKeys, setRandKeys] = useState([]);
+  const [hasWon, setHasWon] = useState(false);
 
   useEffect(() => {
     generateRandKeys();
-  }, []);
+  }, [selectedCards]);
 
   useEffect(() => {
     const updateHighScore = () => {
@@ -21,6 +22,10 @@ const Gameboard = (props) => {
 
     updateHighScore();
   }, [props, props.currentScore]);
+
+  useEffect(() => {
+    checkForWin();
+  });
 
   const randomNumber = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
@@ -34,6 +39,7 @@ const Gameboard = (props) => {
   const chooseCard = (e) => {
     const array = [...selectedCards];
     array.push(e.target.id);
+    console.log(array);
     setSelectedCards(array);
   };
 
@@ -43,8 +49,37 @@ const Gameboard = (props) => {
       props.setCurrentScore(props.currentScore + 1);
     } else {
       props.setCurrentScore(0);
+      setSelectedCards([]);
     }
   };
+
+  const checkForWin = () => {
+    if (selectedCards.length === fruits.length) {
+      setHasWon(true);
+    }
+  };
+
+  const getIndexOfObjectArray = (array, key) => {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i].name === key) return i;
+    }
+    return -1;
+  };
+
+  const addExistingChoice = () => {
+    const randInt = randomNumber(0, selectedCards.length - 1);
+    return getIndexOfObjectArray(fruits, selectedCards[randInt]);
+  };
+
+  const addUnselectedChoice = () => {
+    let randInt = randomNumber(0, fruits.length - 1);
+
+    while (getIndexOfObjectArray(fruits, selectedCards[randInt]) !== -1) {
+      randInt = randomNumber(0, fruits.length - 1);
+    }
+    return randInt;
+  };
+
   const generateRandKeys = () => {
     const cardCount = 6;
     const randKeys = [];
@@ -55,24 +90,51 @@ const Gameboard = (props) => {
       }
       randKeys.push(randInt);
     }
+    if (selectedCards.length > 0) {
+      const existingChoice = addExistingChoice();
+      if (randKeys.indexOf(existingChoice) === -1) {
+        randKeys[randomNumber(0, randKeys.length)] = existingChoice;
+      }
+    }
+    if (selectedCards.length < fruits.length) {
+      const newChoice = addUnselectedChoice();
+      if (randKeys.indexOf(newChoice) === -1) {
+        randKeys[randomNumber(0, randKeys.length)] = newChoice;
+      }
+    }
     setRandKeys(randKeys);
+  };
+
+  const resetGame = () => {
+    props.setCurrentScore(0);
+    setSelectedCards([]);
+    setHasWon(false);
   };
 
   const handleClick = (e) => {
     chooseCard(e);
     checkForPoint(e);
     generateRandKeys();
+    checkForWin();
   };
 
+  if (!hasWon) {
+    return (
+      <div>
+        <Cards
+          chooseCard={chooseCard}
+          checkArrays={checkArrays}
+          randomNumber={randomNumber}
+          handleClick={handleClick}
+          randKeys={randKeys}
+        />
+      </div>
+    );
+  }
   return (
     <div>
-      <Cards
-        chooseCard={chooseCard}
-        checkArrays={checkArrays}
-        randomNumber={randomNumber}
-        handleClick={handleClick}
-        randKeys={randKeys}
-      />
+      <div>You won</div>
+      <button onClick={resetGame}>Restart</button>
     </div>
   );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,6 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 const Header = (props) => {
-  // useEffect(() => {
-  //   /* eslint-disable react-hooks/exhaustive-deps */
-  //   const prevHighScore = Number(sessionStorage.getItem('highScore'));
-  //   console.log(prevHighScore);
-  //   props.setHighScore(prevHighScore);
-  // }, [props, props.highScore]);
-
   return (
     <div>
       <div>Score: {props.currentScore}</div>
